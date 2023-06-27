661773344945bdc8e6cf9922457d7b1c8b82d4a6
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-06-27 16:15:08 +0800

    feat:排名表添加数据联动

--- dash-board-ui/packages/BasicComponents/DashBoardScrollRanking/index.vue
+++ dash-board-ui/packages/BasicComponents/DashBoardScrollRanking/index.vue
@@ -9,7 +9,10 @@
         :key="index"
         style="padding: 0;list-style: none;"
       >
-        <li class="chart-right-list">
+        <li
+          class="chart-right-list"
+          @click="linkage(item)"
+        >
           <span
             v-if="index < config.customize.highlightNum"
             class="list-serial-top"
