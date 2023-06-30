a4308ba18bfa3ab8927f6b59e4ef98096e4f6a96
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-06-30 11:00:39 +0800

    feat:修改预览态移动端样式

--- dash-board-ui/packages/js/utils/registerConfig.js
+++ dash-board-ui/packages/js/utils/registerConfig.js
@@ -95,7 +95,7 @@ function registerRouters (config, router) {
       path: config?.routers?.appPreviewUrl,
       name: 'AppPreview',
       component: () =>
-        require.ensure([], () => require('app/DashboardAppRun'))
+        require.ensure([], () => require('packages/AppDashboardRun'))
     },
     {
       path: config?.routers?.bizComponentDesignUrl,
