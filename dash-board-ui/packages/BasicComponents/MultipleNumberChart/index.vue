934ba7630f25aeb7149c1f1b6079f9f064832a9b
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-07-06 17:44:20 +0800

    feat:修复数字卡片的问题

--- dash-board-ui/packages/BasicComponents/MultipleNumberChart/index.vue
+++ dash-board-ui/packages/BasicComponents/MultipleNumberChart/index.vue
@@ -94,6 +94,7 @@ export default {
       return intPartFormat + floatPart
     },
     dataFormatting (config, data) {
+      debugger
       const metricFieldList = config.dataSource.metricFieldList || []
       let multipleDataList = []
       // 当返回的数据状态为成功时
@@ -113,24 +114,25 @@ export default {
         }
       } else {
         // 返回的模拟数据
-        config.customize.customizeList = []
         for (const col in data.columnData) {
           const val = data.columnData[col]
           multipleDataList.push({
             label: val.remark || val.alias,
             value: data.data[0][val.alias]
           })
-          config.customize.customizeList.push({
-            metric: data.data[0][val.alias], // 指标
-            descriptionField: val.remark || val.alias, // 指标名称
-            metricFontSize: 30, // 指标字体大小
-            metricFontWeight: 700, // 指标字体权重
-            metricColor: '', // 指标字体颜色
-            descriptionFontSize: 16, // 描述字体大小
-            descriptionWeight: 400, // 描述字体权重
-            descriptionColor: '', // 描述字体颜色
-            numberFormat: 'kilobit' // 指标格式
-          })
+          if (config.code === config.key) {
+            config.customize.customizeList.push({
+              metric: data.data[0][val.alias], // 指标
+              descriptionField: val.remark || val.alias, // 指标名称
+              metricFontSize: 30, // 指标字体大小
+              metricFontWeight: 700, // 指标字体权重
+              metricColor: '', // 指标字体颜色
+              descriptionFontSize: 16, // 描述字体大小
+              descriptionWeight: 400, // 描述字体权重
+              descriptionColor: '', // 描述字体颜色
+              numberFormat: 'kilobit' // 指标格式
+            })
+          }
         }
       }
       // 对返回的数据进行统一处理
