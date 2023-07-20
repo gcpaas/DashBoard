a76732a82409511eee7ad81ad2a4b4c8ede1be81
hong.yang <hong.yang@ustcinfo.com>
2023-07-20 10:13:54 +0800

    feat: 升级数据集插件版本为1.0.1.2023072001.Alpha；添加数据集删除前检查页面引用
    升级数据集插件版本为1.0.1.2023072001.Alpha；添加数据集删除前检查页面引用

---
+++ DashBoard/dashboard-core/src/main/java/com/gccloud/dashboard/core/module/manage/service/impl/DatasetExtendServiceImpl.java
@@ -0,0 +1,46 @@
+package com.gccloud.dashboard.core.module.manage.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.gccloud.dashboard.core.module.basic.entity.PageEntity;
+import com.gccloud.dashboard.core.module.manage.service.IDashboardPageService;
+import com.gccloud.dataset.extend.dataset.IDatasetExtendService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author hongyang
+ * @version 1.0
+ * @date 2023/7/20 9:54
+ */
+@Order(100)
+@Service("dashboardDatasetExtendService")
+public class DatasetExtendServiceImpl implements IDatasetExtendService {
+
+    @Resource
+    private IDashboardPageService pageService;
+
+    @Override
+    public String deleteCheck(String id) {
+        if (StringUtils.isBlank(id)) {
+            return null;
+        }
+        LambdaQueryWrapper<PageEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.select(PageEntity::getId, PageEntity::getName);
+        // "businessKey": "id",
+        queryWrapper.like(PageEntity::getConfig, "\"businessKey\":\"" + id + "\"");
+        List<PageEntity> list = pageService.list(queryWrapper);
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        String msg = "数据集已被以下页面引用，无法删除：";
+        for (PageEntity page : list) {
+            msg += page.getName() + "、";
+        }
+        msg = msg.substring(0, msg.length() - 1);
+        return msg;
+    }
+}
