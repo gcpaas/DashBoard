8de05183a929153898e5f87c2315a476b33074de
liu.tao3 <liu.tao3@ustcinfo.com>
2023-08-30 10:39:35 +0800

    fix:去除字段选择校验

--- dash-board-ui/packages/DataSetManagement/src/OriginalEditForm.vue
+++ dash-board-ui/packages/DataSetManagement/src/OriginalEditForm.vue
@@ -546,7 +546,7 @@ export default {
         labelIds: [],
         // 以下为config信息
         sourceId: '',
-        repeatStatus: 1,
+        repeatStatus: 0,
         tableName: '',
         fieldInfo: [],
         fieldDesc: {},
