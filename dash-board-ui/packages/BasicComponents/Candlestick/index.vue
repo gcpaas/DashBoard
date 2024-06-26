98a6956441efe9c015e68645763f04cbaa091f8e
liu.shiyi <liu.shiyi@ustcinfo.com>
2024-06-26 17:38:59 +0800

    fix:K线图添加边距设置

--- dash-board-ui/packages/BasicComponents/Candlestick/index.vue
+++ dash-board-ui/packages/BasicComponents/Candlestick/index.vue
@@ -234,6 +234,12 @@ export default {
             type: 'line'
           }
         },
+        grid: {
+          left: config.customize.left || 50,
+          right: config.customize.right || 20,
+          bottom: config.customize.bottom || 60,
+          top: config.customize.top || 20
+        },
         series: [
           {
             type: 'candlestick',
